6b626e4b8c76aac2f4c4913b26fb868eb1eecb9d
kulaizki <kulaizke@gmail.com>
2024-07-15 01:42:36 +0800

    refactor: change ' to &apos;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,12 +23,16 @@ export default function Home() {
     <div className="flex justify-center p-4 md:p-8">
       <div className="flex flex-col items-center gap-8 md:flex-row md:gap-8 md:p-8">
         <div className="flex flex-col gap-8 md:items-center">
-          <h1 className="text-4xl font-bold md:text-6xl">Explore the beauty of each Pokemon</h1>
+          <h1 className="text-4xl font-bold md:text-6xl">
+            Explore the beauty of each Pokemon
+          </h1>
           <p className="max-w-full text-base font-light leading-7 md:max-w-xl md:text-2xl">
-            Gotta catch 'em all!
+            Gotta catch &apos;em all!
           </p>
-          <Button size={'lg'}>
-            <Link href="/pokedex" className="text-base">Go to Pokedex</Link>
+          <Button size={"lg"}>
+            <Link href="/pokedex" className="text-base">
+              Go to Pokedex
+            </Link>
           </Button>
         </div>
       </div>
