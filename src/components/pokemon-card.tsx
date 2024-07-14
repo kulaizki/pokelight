59bd1e1bba8696d1c61e663cd6ceca5b8835e0bb
kulaizki <kulaizke@gmail.com>
2024-07-14 13:18:06 +0800

    feat: add id

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -18,9 +18,14 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
   return (
     <Link href={`/pokemon/${pokemon.name}`}>
       <div className="flex flex-col justify-center items-center gap-4">
-      <div className="flex justify-center items-center mx-auto bg-gradient-to-r from-white via-blue-500 to-purple-700 text-white rounded-lg p-4 gap-4 border-2 border-white transition-all duration-300 ease-in-out hover:border-yellow-500 hover:yellow-lg">          <Image src={imageUrl} alt={pokemon.name} width={200} height={200} />
+        <div className="flex justify-center items-center mx-auto bg-gradient-to-r from-white via-blue-500 to-purple-700 text-white rounded-lg p-4 gap-4 border-2 border-white transition-all duration-300 ease-in-out hover:border-yellow-500 hover:yellow-lg">
+          {" "}
+          <Image src={imageUrl} alt={pokemon.name} width={200} height={200} />
         </div>
-        <h2 className="text-4xl md:text-3xl">{capitalize(pokemon.name)}</h2>
+        <div className="flex gap-2 items-center">
+          <h2 className="text-4xl md:text-xl text-purple-400">#{id}</h2>
+          <h2 className="text-4xl md:text-2xl">{capitalize(pokemon.name)}</h2>
+        </div>
       </div>
     </Link>
   );
