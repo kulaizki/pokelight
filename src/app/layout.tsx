93c047d4fd1aa778dc4db16b14be15841bfa79c6
kulaizki <kulaizke@gmail.com>
2025-04-17 01:05:16 +0800

    refactor: update body background color for improved dark mode aesthetics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={`${inter.className} bg-gray-100 dark:bg-gray-900`}>
+      <body className={`${inter.className} bg-gray-950`}>
         <ThemeProvider
           attribute='class'
           defaultTheme='system'
